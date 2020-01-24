9160d47977d54a0598440cad7a5670db1f33521c
Yannick Côté <yannick.c.pro.a@gmail.com>
2020-01-24 08:46:31 -0500

    init

---
+++ app/src/main/java/ca/csf/mobile2/tp2/question/AskQuestionActivityViewModel.kt
@@ -0,0 +1,11 @@
+package ca.csf.mobile2.tp2.question
+
+import androidx.databinding.BaseObservable
+import org.parceler.Parcel
+import org.parceler.ParcelConstructor
+
+@Parcel(Parcel.Serialization.BEAN)
+class AskQuestionActivityViewModel @ParcelConstructor constructor( )
+    : BaseObservable() {
+
+}
